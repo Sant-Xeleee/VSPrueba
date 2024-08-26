3d989c2e9e9ea25eebc1a540746a229e2e0b6630
Santiago Emi Perez <santi.perezcervera@gmail.com>
2024-08-25 18:11:32 -0600

    Co-authored-by: JesusLaCo <JesusLaCo@users.noreply.github.com>

--- JavaScript.js
+++ JavaScript.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var inicio = document.getElementById("inicio");
     inicio.addEventListener("click", function (event) {
         event.preventDefault();
-        window.location.href = "Page.aspx";
+        window.location.href = "index.html";
     });
 
     var jiu = document.getElementById("jj");
@@ -11,31 +11,31 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = "jiujitsu.aspx"; // Redirige a la página de Jiu-Jitsu
     });
 
-    document.getElementById('btn').addEventListener('click', function(){
-        document.getElementById('form').scrollIntoView({behavior: "smooth"})
+    document.getElementById('btn').addEventListener('click', function () {
+        document.getElementById('form').scrollIntoView({ behavior: "smooth" })
     });
 
     /*=============== SHOW MENU ===============*/
-        const navToggle = document.getElementById('nav-toggle');
-        const navMenu = document.getElementById('nav-menu');
+    const navToggle = document.getElementById('nav-toggle');
+    const navMenu = document.getElementById('nav-menu');
 
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.toggle('active');
-            navToggle.classList.toggle('active');
-        });
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+        navToggle.classList.toggle('active');
     });
+});
 
-    document.getElementById('send').addEventListener('click', function (event) {
-        event.preventDefault(); // Evita que el formulario se envíe normalmente
-    
-        // Obtener los valores del formulario
-        let name = document.getElementById('name').value;
-        let email = document.getElementById('email').value;
-        let asunto = document.getElementById('asunto').value;
-    
-        // Crear el enlace de WhatsApp
-        let whatsappLink = `https://api.whatsapp.com/send?phone=+529992790193&text=Nombre: ${encodeURIComponent(name)}%0AEmail: ${encodeURIComponent(email)}%0AMensaje: ${encodeURIComponent(asunto)}`;
-    
-        // Abrir WhatsApp
-        window.open(whatsappLink, '_blank');
-    });
+document.getElementById('send').addEventListener('click', function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe normalmente
+
+    // Obtener los valores del formulario
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let asunto = document.getElementById('asunto').value;
+
+    // Crear el enlace de WhatsApp
+    let whatsappLink = `https://api.whatsapp.com/send?phone=+529992790193&text=Nombre: ${encodeURIComponent(name)}%0AEmail: ${encodeURIComponent(email)}%0AMensaje: ${encodeURIComponent(asunto)}`;
+
+    // Abrir WhatsApp
+    window.open(whatsappLink, '_blank');
+});
